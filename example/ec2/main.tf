b08a96fa682cea2f9b9dbd2ea175974edec0a4dc
raviteja <teja4360@gmail.com>
2023-05-09 16:43:08 +0530

    Random txt

---
+++ example/ec2/main.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "test"{
+  ami                    = data.aws_ami.ec2_ami
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [var.sg_id]
+
+  tags = {
+    Name = "test"
+  }
+}
+
+
+data "aws_ami" "ec2_ami" {
+  most_recent            = true
+  name_regex             = "Centos-8-DevOps-Practice"
+  owners                 = "973714476881"
+}
+
+variable "sg_id" {}
+
