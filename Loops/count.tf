9a505116b48a677578fdb844b1f41054507e0fc5
raviteja <teja4360@gmail.com>
2023-05-10 19:14:47 +0530

    Random txt

--- Loops/count.tf
+++ Loops/count.tf
@@ -1,29 +1,14 @@
-# resource "null_resource" "null" {
-#   count = 5
-# }
-# resource "null_resource" "null_ins" {
-#   count = length(var.fruits)
-#   provisioner "local_exec" {
-#     command = "echo fruit name : ${var.fruits[count.index]}"
-#   }
-# }
-#
-# variable "fuits" {
-#   default = [ "apple", "banana", "orange" ]
-# }
-resource "null_resource" "null" {
-  count = 10
-}
+ resource "null_resource" "null" {
+   count = 5
+ }
+ resource "null_resource" "null_ins" {
+   count = length(var.fruits)
+   provisioner "local_exec" {
+     command = "echo fruit name : ${var.fruits[count.index]}"
+   }
+ }
 
-resource "null_resource" "fruits" {
-  count = length(var.fruits)
 
-  provisioner "local-exec" {
-    command = "echo Fruit Name -  ${var.fruits[count.index]}"
-  }
-
-}
-
-variable "fruits" {
-  default = ["apple", "banana", "orange"]
-}
+ variable "fruits" {
+   default = ["apple", "banana", "orange"]
+ }
