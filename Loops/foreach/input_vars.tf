ce6dfb1365549072f2d97c48c47cc63b2af458b8
raviteja <teja4360@gmail.com>
2023-05-10 20:46:29 +0530

    Random txt

--- Loops/foreach/input_vars.tf
+++ Loops/foreach/input_vars.tf
@@ -1,14 +1,14 @@
  variable "fruits" {
    default = {
-     fruit1 {
+     fruit1 = {
        name = "apple"
        count = 10
      }
-     fruit2 {
+     fruit2 = {
        name = "banana"
        count = 20
      }
-     fruit3 {
+     fruit3 = {
        name = "orange"
        count = 10
      }
