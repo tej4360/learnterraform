4ce950d5f7d32d215f99f71d94cee32cb608997f
raviteja <teja4360@gmail.com>
2023-05-10 19:28:32 +0530

    Random txt

--- Loops/count.tf
+++ Loops/count.tf
@@ -1,13 +1,13 @@
  resource "null_resource" "null" {
    count = 10
  }
+
  resource "null_resource" "fruits" {
    count = length(var.fruits)
    provisioner "local_exec" {
      command = "echo fruit name : ${var.fruits[count.index]}"
    }
  }
-
 
  variable "fruits" {
    default = ["apple", "banana", "orange"]
