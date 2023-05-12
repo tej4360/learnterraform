b7d73826e70336adc72153313dd61806e0fc8cfe
raviteja <teja4360@gmail.com>
2023-05-12 12:18:25 +0530

    Random txt

---
+++ roboshop_terraform/servers.tf
@@ -0,0 +1,22 @@
+resource "aws_instance" "frontend" {
+  ami                    = data.aws_ami.ec2_ami.image_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [var.sg_id]
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+data "aws_ami" "ec2_ami" {
+  most_recent            = true
+  name_regex             = "Centos-8-DevOps-Practice"
+  owners                 = ["973714476881"]
+}
+
+
+variable "security_group_id" {}
+
+data "aws_security_group" "selected" {
+  id = var.security_group_id
+}
