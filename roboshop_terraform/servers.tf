d0df598cd82b49b7024f71a33a17e154b69e0d2e
raviteja <teja4360@gmail.com>
2023-05-12 12:39:52 +0530

    Random txt

--- roboshop_terraform/servers.tf
+++ roboshop_terraform/servers.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "frontend" {
   ami                    = data.aws_ami.ec2_ami.image_id
   instance_type          = var.instance_type
-  vpc_security_group_ids = data.aws_security_group.selected.aws_security_group
+  vpc_security_group_ids = data.aws_security_group.allow-all.id
 
   tags = {
     Name = "frontend"
@@ -14,6 +14,6 @@ data "aws_ami" "ec2_ami" {
   owners                 = ["973714476881"]
 }
 
-data "aws_security_group" "selected" {
-  id = var.security_group_id
+data "aws_security_group" "allow-all" {
+  name = "allow-all"
 }
